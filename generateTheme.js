46dd0fdad2927f1ab4c01a64b1bed4f40f48f2a4
VChet <17050347+VChet@users.noreply.github.com>
2019-11-11 00:09:51 +0500

    refactor(color mappings): move mappings to separate file

--- generateTheme.js
+++ generateTheme.js
@@ -3,8 +3,6 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const paletteFile = "vanilla-dark";
-
 const themes = {
   // aqua: "./themes/aqua.json",
   cream: "./themes/cream.json",
@@ -18,80 +16,10 @@ if (!themes[themeName]) {
   console.log(chalk.yellow("Theme name wasn't specified or was incorrect. Generating `Cream` theme."));
   console.log(chalk.yellow(`Available themes: ${Object.keys(themes)}`));
 }
-const theme = require(themes[themeName]);
 
-const mappings = {
-  windowBg: "GRAY",
-  windowBgOver: "GRAY_LIGHT",
-  windowBgRipple: "GRAY_SELECTION",
-  lightButtonBg: "GRAY",
-  lightButtonBgOver: "GRAY_LIGHT",
-  outlineButtonBgOver: "GRAY_LIGHT",
-  menuBg: "GRAY",
-  placeholderFg: "LIGHT",
-  placeholderFgActive: "TEXT",
-  filterInputBorderFg: "GRAY_LIGHT",
-  filterInputInactiveBg: "GRAY_LIGHT",
-  titleBg: "GRAY_LIGHT",
-  trayCounterBg: "MAIN",
-  trayCounterBgMute: "MAIN_DARK",
-  trayCounterFg: "GRAY_DARK",
-  trayCounterBgMacInvert: "GRAY_DARK",
-  trayCounterFgMacInvert: "GRAY_DARK_TP",
-  boxSearchBg: "GRAY",
-  contactsBgOver: "GRAY",
-  dialogsNameFg: "LIGHT",
-  dialogsDateFg: "#B3B3B3",
-  dialogsTextFg: "LIGHT",
-  dialogsVerifiedIconFg: "GRAY",
-  dialogsUnreadBgMuted: "MAIN_DARK",
-  dialogsUnreadBg: "MAIN",
-  dialogsUnreadFg: "GRAY_DARK",
-  dialogsBgOver: "GRAY_LIGHT",
-  dialogsNameFgOver: "LIGHT",
-  dialogsDateFgOver: "#B3B3B3",
-  dialogsUnreadBgOver: "MAIN_DARK",
-  dialogsUnreadBgMutedOver: "MAIN",
-  dialogsBgActive: "MAIN_DARK",
-  dialogsRippleBg: "GRAY_SELECTION",
-  dialogsRippleBgActive: "MAIN",
-  searchedBarBg: "GRAY_LIGHT",
-  topBarBg: "GRAY",
-  emojiPanCategories: "GRAY",
-  historyTextInFg: "LIGHT",
-  historyTextInFgSelected: "#B3B3B3",
-  historyTextOutFg: "LIGHT",
-  historyTextOutFgSelected: "historyTextInFgSelected",
-  historyUnreadBarBg: "GRAY_LIGHT",
-  msgInBg: "GRAY_LIGHT",
-  msgInBgSelected: "#484848",
-  msgOutBg: "GRAY_LIGHT",
-  msgOutBgSelected: "#484848",
-  msgOutServiceFg: "MAIN",
-  msgInDateFg: "LIGHT",
-  msgInDateFgSelected: "msgInDateFg",
-  msgOutDateFg: "msgInDateFg",
-  msgOutDateFgSelected: "msgInDateFgSelected",
-  msgServiceFg: "LIGHT",
-  msgServiceBg: "GRAY_DARK",
-  msgServiceBgSelected: "MAIN_DARK",
-  msgInMonoFg: "#EFEBDA",
-  msgFileOutBg: "MAIN",
-  historyFileInIconFg: "GRAY_LIGHT",
-  historyFileInIconFgSelected: "MAIN_DARK",
-  historyFileInRadialFg: "GRAY_LIGHT",
-  historyFileOutIconFg: "GRAY_LIGHT",
-  historyFileOutIconFgSelected: "MAIN_DARK",
-  historyFileOutRadialFgSelected: "MAIN_DARK",
-  msgWaveformOutActive: "MAIN",
-  historyToDownBg: "GRAY",
-  historyToDownBgOver: "GRAY_LIGHT",
-  historyToDownBgRipple: "GRAY_SELECTION",
-  historyComposeAreaBg: "GRAY",
-  mainMenuCoverBg: "MAIN_DARK",
-  filterInputActiveBg: "GRAY_LIGHT",
-  botKbBg: "GRAY_LIGHT",
-};
+const theme = require(themes[themeName]);
+const paletteFile = "vanilla-dark";
+const mappings = require("./mappings").mappings;
 
 fs.readFile(`${paletteFile}_original.tdesktop-palette`, "utf8", function(error, data) {
   if (error) return console.log(error);
