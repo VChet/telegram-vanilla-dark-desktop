e582837ef6fa305ff41f57af4985b7f22d6009c7
VChet <17050347+VChet@users.noreply.github.com>
2020-03-15 00:32:36 +0500

    refactor: load all themes at the script start, move themes to one file

--- generateTheme.js
+++ generateTheme.js
@@ -1,37 +1,20 @@
-"use strict";
-
 const fs = require("fs");
-const path = require("path");
 const chalk = require("chalk");
 
 const { mappings } = require("./mappings");
+const { themes } = require("./themes");
 const { version } = require("./package.json");
-const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-
-function getThemes() {
-  const files = fs.readdirSync("./themes/");
-  const themes = [];
-  files.map((file) => {
-    if (path.extname(file) === ".json") {
-      themes.push({
-        name: capitalize(path.basename(file, ".json")),
-        path: `./themes/${file}`
-      });
-    }
-  });
-  return themes;
-}
 
 function generatePalette(theme) {
   const originalPalette = fs.readFileSync(`original.tdesktop-palette`, "utf8");
   const lines = originalPalette.split("\n").map(line => {
     const constant = line.substring(0, line.indexOf(":"));
     if (mappings[constant]) {
-      line = line.replace(/[#][^;]+/gm, mappings[constant]);
+      return line.replace(/[#][^;]+/gm, mappings[constant]);
     }
     return line;
   });
-  const themeMappings = Object.entries(require(theme.path)).map(line => line.join(": ")).join(";\n") + ";";
+  const themeMappings = `${Object.entries(theme.constants).map(line => line.join(": ")).join(";\n")};`;
 
   let result = `// Telegram Vanilla Dark ${theme.name} (${version})\n`;
   result += lines.join("\n").replace("{{THEME}}", themeMappings);
@@ -39,15 +22,14 @@ function generatePalette(theme) {
 }
 
 const themeInput = process.argv[2];
-const themes = getThemes();
 
 if (themeInput === "all") {
   themes.map((theme) => generatePalette(theme));
-  return;
+  process.exit();
 }
 try {
-  const theme = themes.find((theme) => theme.name.toLowerCase() === themeInput.toLowerCase());
-  generatePalette(theme);
+  const themeData = themes.find((theme) => theme.name.toLowerCase() === themeInput.toLowerCase());
+  generatePalette(themeData);
 } catch (error) {
   const availableThemes = themes.map((theme) => theme.name).join(", ");
   console.log(chalk.red("Aborting: theme name wasn't specified or was incorrect."));
---
+++ themes.js
@@ -0,0 +1,61 @@
+exports.themes = [{
+  name: "Aqua",
+  constants: {
+    MAIN_DARK: "#5B8E90",
+    MAIN: "#77BABC",
+    MAIN_OVERLAY: "#77BABC4C",
+    MAIN_LIGHT: "#8ED1CC",
+    GRAY_DARK: "#1D1D24",
+    GRAY_DARK_TP: "#1D1D2401",
+    GRAY: "#23232D",
+    GRAY_LIGHT: "#2C2C36",
+    GRAY_SELECTION: "#31313B",
+    TEXT: "#AAAAAA",
+    LIGHT: "#CCCCCC"
+  }
+}, {
+  name: "Cream",
+  constants: {
+    MAIN_DARK: "#908B67",
+    MAIN: "#B9B384",
+    MAIN_OVERLAY: "#B9B3844C",
+    MAIN_LIGHT: "#D0CA94",
+    GRAY_DARK: "#191919",
+    GRAY_DARK_TP: "#19191901",
+    GRAY: "#222222",
+    GRAY_LIGHT: "#272727",
+    GRAY_SELECTION: "#2C2C2C",
+    TEXT: "#AAAAAA",
+    LIGHT: "#CCCCCC"
+  }
+}, {
+  name: "Green",
+  constants: {
+    MAIN_DARK: "#4B8B60",
+    MAIN: "#63B97F",
+    MAIN_OVERLAY: "#63B97F4C",
+    MAIN_LIGHT: "#7AD08F",
+    GRAY_DARK: "#19191A",
+    GRAY_DARK_TP: "#19191A01",
+    GRAY: "#212122",
+    GRAY_LIGHT: "#2A2A2C",
+    GRAY_SELECTION: "#2F2F31",
+    TEXT: "#AAAAAA",
+    LIGHT: "#CCCCCC"
+  },
+}, {
+  name: "Red",
+  constants: {
+    MAIN_DARK: "#B35843",
+    MAIN: "#FF7B5D",
+    MAIN_OVERLAY: "#FF7B5D4C",
+    MAIN_LIGHT: "#FF926D",
+    GRAY_DARK: "#262A2E",
+    GRAY_DARK_TP: "#262A2E01",
+    GRAY: "#2B3034",
+    GRAY_LIGHT: "#2F353A",
+    GRAY_SELECTION: "#33393E",
+    TEXT: "#AAAAAA",
+    LIGHT: "#CCCCCC"
+  }
+}];
