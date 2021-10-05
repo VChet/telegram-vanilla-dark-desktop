c46009647bea8efdb67939a3f5ea7ecbf43cd82a
VChet <17050347+VChet@users.noreply.github.com>
2021-10-05 14:12:27 +0500

    chore: update dependencies, replace nanocolors with picocolors

--- src/generateTheme.ts
+++ src/generateTheme.ts
@@ -1,5 +1,5 @@
 import { readFileSync, writeFileSync } from "fs";
-import { red, yellow, green } from "nanocolors";
+import { red, yellow, green } from "picocolors";
 
 import mappings from "./mappings";
 import themes from "./themes";
