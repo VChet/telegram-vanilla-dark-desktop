c532bbeaab53894624f616f81710b1f8536765d9
VChet <17050347+VChet@users.noreply.github.com>
2021-12-01 23:20:59 +0500

    feat(generator): add background tiles generator
    closes #1

--- src/generateTheme.ts
+++ src/generateTheme.ts
@@ -1,47 +1,57 @@
-import { readFileSync, writeFileSync } from "fs";
-import { red, yellow, green } from "picocolors";
-
+import { createCanvas } from "canvas";
+import { readFile, writeFile } from "fs/promises";
+import { green, red, yellow } from "picocolors";
+import { version } from "../package.json";
 import mappings from "./mappings";
 import themes from "./themes";
-import { version } from "../package.json";
-
 import { Theme } from "./types/Theme";
 
-function generatePalette(theme: Theme): void {
-  const originalPalette: string = readFileSync("./src/palettes/original.tdesktop-palette", "utf8");
+async function generatePalette({ name, constants }: Theme): Promise<void> {
+  const originalPalette: string = await readFile("./src/palettes/original.tdesktop-palette", "utf8");
   const lines: Array<string> = originalPalette.split("\n").map((line) => {
     const constant = line.substring(0, line.indexOf(":"));
     return mappings[constant] ?
       line.replace(/[#][^;]+/gm, mappings[constant]) :
       line;
   });
-  const themeMappings = `${Object.entries(theme.constants).map((line) => line.join(": ")).join(";\n")};`;
+  const themeMappings = `${Object.entries(constants).map((line) => line.join(": ")).join(";\n")};`;
 
-  let result = `// Telegram Vanilla Dark ${theme.name} (${version})\n`;
+  let result = `// Telegram Vanilla Dark ${name} (${version})\n`;
   result += lines.join("\n").replace("{{THEME}}", themeMappings);
-  writeFileSync(`./src/palettes/vanilla-dark_${theme.name}.tdesktop-palette`, result, "utf8");
+  return writeFile(`./src/palettes/vanilla-dark_${name}.tdesktop-palette`, result, "utf8");
 }
 
-const themeInput: string | undefined = process.argv[2];
-const availableThemes = themes.map((theme) => theme.name).join(", ");
-
-if (!themeInput) {
-  console.log(red("Aborting: theme name not specified."));
-  console.log(yellow(`Available themes: ${availableThemes}`));
-  process.exit();
+function generateBackground({ name, constants }: Theme): Promise<void> {
+  const size = 16;
+  const canvas = createCanvas(size, size);
+  const context = canvas.getContext("2d");
+  context.fillStyle = constants.GRAY_DARK;
+  context.fillRect(0, 0, size, size);
+  return writeFile(`./src/backgrounds/bgTile_${name}.png`, canvas.toBuffer("image/png"));
 }
 
-if (themeInput === "all") {
-  themes.map((theme) => generatePalette(theme));
-  console.log(green("Generation finished!"));
-  process.exit();
-}
+(async () => {
+  const themeInput: string | undefined = process.argv[2];
+  const availableThemes = themes.map((theme) => theme.name).join(", ");
 
-const themeData = themes.find((theme) => theme.name.toLowerCase() === themeInput.toLowerCase());
-if (themeData) {
-  generatePalette(themeData);
-  console.log(green("Generation finished!"));
-} else {
-  console.log(red("Aborting: incorrect theme name."));
-  console.log(yellow(`Available themes: ${availableThemes}`));
-}
+  if (!themeInput) {
+    console.log(red("Aborting: theme name not specified."));
+    console.log(yellow(`Available themes: ${availableThemes}`));
+    process.exit();
+  }
+
+  if (themeInput === "all") {
+    await Promise.all(themes.map((theme) => Promise.all([generatePalette(theme), generateBackground(theme)])));
+    console.log(green("Generation finished!"));
+    process.exit();
+  }
+
+  const themeData = themes.find((theme) => theme.name.toLowerCase() === themeInput.toLowerCase());
+  if (themeData) {
+    await Promise.all([generatePalette(themeData), generateBackground(themeData)]);
+    console.log(green("Generation finished!"));
+  } else {
+    console.log(red("Aborting: incorrect theme name."));
+    console.log(yellow(`Available themes: ${availableThemes}`));
+  }
+})();
