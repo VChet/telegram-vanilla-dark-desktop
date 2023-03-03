ebdab6a601f7ff6de721e4edcafa97219ae187b7
VChet <17050347+VChet@users.noreply.github.com>
2023-03-03 23:30:49 +0600

    chore: add bump script

---
+++ src/bump.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { execSync } from "child_process";
+
+function main() {
+  const palettesDir = path.join(__dirname, "palettes");
+  const files = fs.readdirSync(palettesDir, { withFileTypes: true })
+    .filter((file) => file.isFile() && file.name.match(/^vanilla-dark_.*\.tdesktop-palette$/))
+    .map((file) => path.join(palettesDir, file.name));
+
+  files.forEach((filename) => {
+    const contents = fs.readFileSync(filename, "utf8");
+    const updatedContents = contents.replace(/\b\d+\.\d+\.\d+\b/g, process.env.npm_package_version as string);
+    fs.writeFileSync(filename, updatedContents, "utf8");
+  });
+
+  execSync("git add .");
+}
+
+main();
