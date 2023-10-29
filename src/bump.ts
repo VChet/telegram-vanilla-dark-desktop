1b2519930df49be60929f3b7c59efd9b7a408b74
VChet <17050347+VChet@users.noreply.github.com>
2023-10-29 16:36:21 +0400

    refactor(bump): update file regex

--- src/bump.ts
+++ src/bump.ts
@@ -5,7 +5,7 @@ import { execSync } from "child_process";
 function main() {
   const palettesDir = path.join(__dirname, "palettes");
   const files = fs.readdirSync(palettesDir, { withFileTypes: true })
-    .filter((file) => file.isFile() && file.name.match(/^vanilla-dark_.*\.tdesktop-palette$/))
+    .filter((file) => file.isFile() && /^vanilla-dark_.*\.tdesktop-palette$/.exec(file.name))
     .map((file) => path.join(palettesDir, file.name));
 
   files.forEach((filename) => {
