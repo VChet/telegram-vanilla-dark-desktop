21e406a50e2fa51dd3b90ae3d147366790ba3fbe
VChet <17050347+VChet@users.noreply.github.com>
2023-10-29 16:38:35 +0400

    refactor(bump): extract from function, update iterator

--- src/bump.ts
+++ src/bump.ts
@@ -2,19 +2,15 @@ import fs from "fs";
 import path from "path";
 import { execSync } from "child_process";
 
-function main() {
-  const palettesDir = path.join(__dirname, "palettes");
-  const files = fs.readdirSync(palettesDir, { withFileTypes: true })
-    .filter((file) => file.isFile() && /^vanilla-dark_.*\.tdesktop-palette$/.exec(file.name))
-    .map((file) => path.join(palettesDir, file.name));
+const palettesDir = path.join(__dirname, "palettes");
+const files = fs.readdirSync(palettesDir, { withFileTypes: true })
+  .filter((file) => file.isFile() && /^vanilla-dark_.*\.tdesktop-palette$/.exec(file.name))
+  .map((file) => path.join(palettesDir, file.name));
 
-  files.forEach((filename) => {
-    const contents = fs.readFileSync(filename, "utf8");
-    const updatedContents = contents.replace(/\b\d+\.\d+\.\d+\b/g, process.env.npm_package_version as string);
-    fs.writeFileSync(filename, updatedContents, "utf8");
-  });
-
-  execSync("git add .");
+for (const filename of files) {
+  const contents = fs.readFileSync(filename, "utf8");
+  const updatedContents = contents.replace(/\b\d+\.\d+\.\d+\b/g, process.env.npm_package_version as string);
+  fs.writeFileSync(filename, updatedContents, "utf8");
 }
 
-main();
+execSync("git add .");
